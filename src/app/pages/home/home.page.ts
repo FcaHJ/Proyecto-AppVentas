2a9238de110743d2ac26fa0f632a4c891032b4aa
FcaHJ <fran.herrera@duocuc.cl>
2024-09-11 20:24:05 -0300

    implementa animacion

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,17 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalComponent } from 'src/app/components/modal/modal.component';
 
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter, :leave', [
+        animate('2.5s ease-in-out')
+      ]),
+    ]),
+  ]
 })
 export class HomePage implements OnInit {
-
-  constructor(private modalCtrl: ModalController) { }
-
-  ngOnInit() {
+  
+  constructor(private modalCtrl: ModalController) { 
   }
+
+  ngOnInit(){
+    }
 
   async openModal() {
     const modal = await this.modalCtrl.create({
