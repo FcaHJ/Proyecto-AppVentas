ec8ce601d52c814163463f9d07ff2db69fb46705
FcaHJ <fran.herrera@duocuc.cl>
2024-09-11 20:23:46 -0300

    modificacion

--- src/app/api/calculo.service.ts
+++ src/app/api/calculo.service.ts
@@ -11,12 +11,6 @@ export class CalculoService {
     sector3: 70000,
     vip: 100000,
   }
-  cantidad: Record<string, number> = {
-    sector1: 0,
-    sector2: 0,
-    sector3: 0,
-    vip: 0,
-  };
 
   total: number = 0;
   desc: number = 0;
@@ -25,20 +19,17 @@ export class CalculoService {
 
   constructor() { }
 
-  setEdad(edad: number){
-    this.edad = edad
-  }
+  calcularTotal(cantidad: Record<string, number>, edad: number):{total: number, desc: number, totalDesc: number } {
+    let total = 0;
+    let desc = 0;
+    let totalDesc = 0;
 
-  setCantidad(cantidad: Record<string,number>){
-    this.cantidad = cantidad
-  }
-
-  calcularTotal() {
     this.total = 
-      this.cantidad[2] * this.precios.sector1 +
-      this.cantidad[2] * this.precios.sector2 +
-      this.cantidad[2] * this.precios.sector3 +
-      this.cantidad[2] * this.precios.vip;
+      cantidad['sector1'] * this.precios.sector1 +
+      cantidad['sector2'] * this.precios.sector2 +
+      cantidad['sector3'] * this.precios.sector3 +
+      cantidad['vip'] * this.precios.vip;
+
     // Aplicar descuento si la edad es menor que 18 o mayor que 60
     if (this.edad < 18) {
       this.desc = this.total * 0.1;
@@ -50,21 +41,7 @@ export class CalculoService {
       this.desc = 0;
       this.totalDesc = this.total;
     }
+    return {total,desc,totalDesc};
   }
-
-    // Obtener el total
-    getTotal() {
-      return this.total;
-    }
-
-    // Obtener el descuento
-    getDescuento() {
-      return this.desc;
-    }
-
-    // Obtener el total con descuento
-    getTotalDescuento() {
-      return this.totalDesc;
-    }
 }
 
